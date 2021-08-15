25b4b225965e6ee667f12ea49613572fa6df5f46
杜宇飞 <df>
2021-08-15 21:22:46 +0800

    创建注册中心服务提供者

---
+++ eureka/src/main/java/com/engineer/app/Application.java
@@ -0,0 +1,24 @@
+package com.engineer.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 杜宇飞
+ * @Description: 注册中心启动类
+ * @date 2021年08月15日 -- 20:28
+ * @cpoyright: ****科技有限公司
+ ***/
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableEurekaClient
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+}
